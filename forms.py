dc32c2ce3523fb69d7dd633dcd792f5d5304b5c3
Alex Andrew <Alex Andrew>
2021-12-21 01:51:19 +0000

    login, register + home styling done

--- forms.py
+++ forms.py
@@ -28,7 +28,7 @@ class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    submit = SubmitField('Log In')
 
 
 
